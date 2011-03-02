e938c30ae82e094b0d213aa9624881157eaec1a0
souto <soujuegos@hotmail.com>
2011-03-02 17:40:39 +0100

    activa gestionada con desplegable

--- application/views/peliculas_editar.php
+++ application/views/peliculas_editar.php
@@ -6,22 +6,25 @@
   </head>
   <body>
     <?= form_open('usuarios/logout') ?>
-      <p align="right">Usuario: <?= $usuario ?>
+      <p align="right">Pelicula: <?= $pelicula ?>
       <?= form_submit('salir', 'Salir') ?>
       </p>
     <?= form_close() ?><hr/>
     <p><?= validation_errors() ?></p>
     <p><?= form_open('peliculas/editar') ?>
-      <?= form_label('Número:', 'numero') ?>
-      <?= form_input('numero', set_value('numero', $numero), 'readonly') ?><br/>
+      <?= form_label('Código:', 'codigo') ?>
+      <?= form_input('codigo', set_value('codigo', $codigo), 'readonly') ?><br/>
       <?= form_label('Titulo:', 'titulo') ?>
       <?= form_input('titulo', set_value('titulo', $titulo)) ?><br/>
-      <?= form_label('Precio de compra:', 'precio_compra') ?>
-      <?= form_input('precio_compra', set_value('precio_compra', $precio_compra)) ?><br/>
-      <?= form_label('Precio de alquiler:', 'precio_alquiler') ?>
-      <?= form_input('precio_alquiler', set_value('precio_alquiler', $precio_alquiler)) ?><br/>
+      <?= form_label('Precio de alquiler:', 'precio_alq') ?>
+      <?= form_input('precio_alq', set_value('precio_alq', $precio_alquiler)) ?><br/>
+      <?= form_label('Fecha de alta:', 'fech_alt_pel') ?>
+      <?= form_input('fech_alt_pel', set_value('fech_alt_pel', $fech_alt_pel)) ?><br/>
       <?= form_label('Activa:', 'activa') ?>
-      <?= form_input('activa', set_value('activa', $activa)) ?><br/>
+      <select name="Activa">
+        <option value="TRUE" <?php echo set_select('myselect', 'one', $activa); ?> >Si</option>
+        <option value="FALSE" <?php echo set_select('myselect', 'two', !$activa); ?> >No</option>
+      </select> 
       <?= form_submit('editar', 'Editar') ?>
       <?= anchor('socios/index', 'Volver') ?>
     <?= form_close() ?></p>
