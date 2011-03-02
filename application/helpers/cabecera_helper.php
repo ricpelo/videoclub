47288df43bf8a8d189bb59c765121c823d186010
Carlos Villalón <leonardooo13@hotmail.com>
2011-03-02 19:44:14 +0100

    actualizando cabecera

--- application/helpers/cabecera_helper.php
+++ application/helpers/cabecera_helper.php
@@ -10,6 +10,6 @@ if ( ! function_exists('cabecera'))
 {
 	function cabecera()
 	{
-		return "<h1>". anchor('socios/index/', 'Socios //') . " " . anchor('peliculas/index/', 'Peliculas //') . " " . anchor('alquileres/index/', 'Alquileres') . "</h>";
+		return "<h3>". anchor('socios/index/', 'Socios //') . " " . anchor('peliculas/index/', 'Peliculas //') . " " . anchor('alquileres/index/', 'Alquileres') . " " . form_open('usuarios/logout') . "<p align=\"right\">Usuario:" . $usuario . form_submit('salir', 'Salir') . "</p>" . form_close() "</h3>";
 	}
 }
