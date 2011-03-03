718d23b9592cf5f64e5a5c57279e7281f23dbff9
dosdai <sdrxoo@gmail.com>
2011-03-03 17:47:15 +0100

    Arreglado conflictos en peliculas_crear y usuarios_crear

--- application/views/usuarios_crear.php
+++ application/views/usuarios_crear.php
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <title>Crear nuevo usuario</title>
-  </head>
-  <body>
-    <p><?= $une ?></p>
-    <p><?= form_open('usuarios/crear') ?>
-      <?= form_label('Nombre:', 'nombre') ?>
-      <?= form_input('nombre', set_value('nombre')) ?><br/>
-      <?= form_label('Contraseña:', 'password') ?>
-      <?= form_input('password', set_value('password')) ?><br/>
-      <?= form_submit('crear', 'Crear') ?>
-    <?= form_close() ?></p>
-  </body>
-</html>
-
-=======
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
   <head>
@@ -44,5 +24,3 @@
   </div>
   </body>
 </html>
-
->>>>>>> b9b98b134a11c089ee28de16753e3c3a20303469
