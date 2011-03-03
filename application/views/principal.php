9f1f7cd717e8f12edea84b578fff186b5eda8dde
Leandro Harana <leandro_hy@hotmail.com>
2011-03-03 18:07:29 +0100

    actualizar

--- application/views/principal.php
+++ application/views/principal.php
@@ -3,28 +3,6 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <title>Menú principal</title>
-  </head>
-  <body>
-	 <?= cabecera() ?>
-
-   
-	 <p align="center"><h2>Gestión Videoclub</h2></p><br>
-         <p align="center"><?= anchor('socios/index/','Ir a gestión de socio') ?></td> <p/>
- 	 <p align="center"><?= anchor('peliculas/index/','Ir a gestión de peliculas') ?></td>
-	<p align="center"><?= anchor('alquileres/index/','Ir a gestión de Alquileres') ?></td>
- <p/>
-	
-
-      
-            
-           
-  </body>
-</html>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <title>Menú principal</title>
   <link href="../estilos/videoclub.css" rel="stylesheet" type="text/css">
   </head>
   <body>
