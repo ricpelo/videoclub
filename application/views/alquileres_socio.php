003891fd3e6c0f58a9cb30f29a5c47d9708ed953
Carlos Villalón <leonardooo13@hotmail.com>
2011-03-04 19:15:54 +0100

    Alquileres actualizados

--- application/views/alquileres_socio.php
+++ application/views/alquileres_socio.php
@@ -1,16 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <title>Gestión de alquileres - Alquileres de <?= $nombre." ".$apellido ?></title>
-  </head>
-  <body>
-      <div id="principal">
-	<div id="cabecera">
-	  <h1>Gestión de Videoclub</h1>
-	  <?= cabecera() ?>
-	</div>
-    <div id="contenido">
+
   <p><?= validation_errors() ?></p>
   <?= form_open('alquileres/seleccionar_socio') ?>
   <?= form_label('Número:', 'numero') ?>
@@ -67,8 +55,5 @@
   </div>
   <?php endif; ?>
     </div>    
-    <div id="pie_pag">&copy; Alumnos de 2º DAI, IES Doñana, 2011</div>
-  </div>
-  </body>
-</html>
+   
 
