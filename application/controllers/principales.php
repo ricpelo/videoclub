320b9e358c9c807bec7419eb88eda5f9c6e3891c
Carlos Villalón <leonardooo13@hotmail.com>
2011-03-07 16:15:26 +0100

    Controlador principales actualizado

--- application/controllers/principales.php
+++ application/controllers/principales.php
@@ -4,6 +4,9 @@ class Principales extends CI_Controller {
 
   function __construct() {
     parent::__construct();
+      if (!$this->session->userdata('usuario')) {
+      redirect('usuarios/login');
+    }
   }
    function index(){
 	$this->load->helper(array('form','url'));
