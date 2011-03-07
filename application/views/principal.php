ddb5f2487f9a45938d7837eb56eb1d07dc5b31f1
Mariqui Romero <mariquir@gmail.com>
2011-03-07 16:29:44 +0100

    cambios fallitos y estéticos

--- application/views/principal.php
+++ application/views/principal.php
@@ -1,6 +1,53 @@
+<div id = "novedades">
+	<!-- <p><?= anchor('socios/index/','Ir a gestión de socio') ?> <p/> --> 
+ 	<!-- <p><?= anchor('peliculas/index/','Ir a gestión de peliculas') ?><p/> -->
+<fieldset style="border:1px solid #006291" align = center><legend><h1>Novedades en el videoclub</h1></legend>
+<table width = 100% align = center>  
+<tr><td><div>
+  <table width="200" border="0" align="center">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
 
+  </div></td>&nbsp
+<td><div>
+  <table width="200" border="0" align="center">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
 
-	 <p><?= anchor('socios/index/','Ir a gestión de socio') ?> <p/>
- 	 <p><?= anchor('peliculas/index/','Ir a gestión de peliculas') ?><p/>
+  </div></td>&nbsp
+<td><div>
+  <table width="200" border="0" align="center">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+
+  </div></td></tr>
+</table>
+</fieldset>
+</div>
+   
    
 
